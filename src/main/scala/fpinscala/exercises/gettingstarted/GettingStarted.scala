ce9ebe759a3afacca95d336ab544cc3c06e2f0a3
Sean Wibisono <sean@autumncompass.com>
2026-01-12 16:14:43 +1100

    gettingstarted

--- src/main/scala/fpinscala/exercises/gettingstarted/GettingStarted.scala
+++ src/main/scala/fpinscala/exercises/gettingstarted/GettingStarted.scala
@@ -32,8 +32,14 @@ object MyProgram:
     acc
 
   // Exercise 1: Write a function to compute the nth fibonacci number
+  def fib(n: Int): Int = {
+    def go(n: Int, current: Int, next: Int): Int =
+      if n <= 0 then current
+      else go(n - 1, next, current + next)
 
-  def fib(n: Int): Int = ???
+    go(n, 0, 1)
+  }
+
 
   // This definition and `formatAbs` are very similar..
   private def formatFactorial(n: Int) =
@@ -121,7 +127,13 @@ object PolymorphicFunctions:
 
   // Exercise 2: Implement a polymorphic function to check whether
   // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = ???
+  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = {
+    def loop(as: Array[A], index: Int, gt: (A, A) => Boolean): Boolean =
+      if index >= as.length then true
+      else !gt(as(index - 1), as(index)) && loop(as, index + 1, gt)
+
+    loop(as, 1, gt)
+  }
 
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
@@ -134,13 +146,14 @@ object PolymorphicFunctions:
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
   def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+    (a: A) => (b: B) => f(a, b)
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 4: Implement `uncurry`
-  def uncurry[A,B,C](f: A => B => C): (A, B) => C =
-    ???
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C = {
+    (a: A, b: B) => f(a)(b)
+  }
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -155,5 +168,5 @@ object PolymorphicFunctions:
   // Exercise 5: Implement `compose`
 
   def compose[A,B,C](f: B => C, g: A => B): A => C =
-    ???
+    (a: A) => f(g(a))
 
