d68532554f4749d222b0c1b8d5920d406983cde0
Sean Wibisono <sean@autumncompass.com>
2026-01-17 21:58:51 +1100

    option

--- src/main/scala/fpinscala/exercises/errorhandling/Option.scala
+++ src/main/scala/fpinscala/exercises/errorhandling/Option.scala
@@ -2,20 +2,32 @@ package fpinscala.exercises.errorhandling
 
 // Hide std library `Option` since we are writing our own in this chapter
 import scala.{Option as _, Some as _, None as _}
+import scala.math
 
 enum Option[+A]:
   case Some(get: A)
   case None
 
-  def map[B](f: A => B): Option[B] = ???
+  def map[B](f: A => B): Option[B] =
+    this match
+      case None => None
+      case Some(a) => Some(f(a))
 
-  def getOrElse[B>:A](default: => B): B = ???
+  def getOrElse[B>:A](default: => B): B =
+    this match
+      case None => default
+      case Some(a) => a
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    map(f).getOrElse(None)
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = ???
+  def orElse[B>:A](ob: => Option[B]): Option[B] =
+    map(oa => Some(oa)).getOrElse(ob)
 
-  def filter(f: A => Boolean): Option[A] = ???
+  def filter(f: A => Boolean): Option[A] =
+    this match
+      case Some(a) if f(a) => if f(a) then Some(a) else None
+      case _ => None
 
 object Option:
 
@@ -36,10 +48,28 @@ object Option:
     if xs.isEmpty then None
     else Some(xs.sum / xs.length)
 
-  def variance(xs: Seq[Double]): Option[Double] = ???
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a.flatMap(aa => b.flatMap(bb => Some(f(aa, bb))))
 
-  def sequence[A](as: List[Option[A]]): Option[List[A]] = ???
+  def sequence[A](as: List[Option[A]]): Option[List[A]] = {
+    as.foldRight(Some(Nil): Option[List[A]]) { (next, acc) =>
+      acc.flatMap(
+        as => next.flatMap(
+          a => Some(a :: as)
+        )
+      )
+    }
+  }
 
-  def traverse[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] = ???
+  def traverse[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] =
+    as.foldRight(Some(Nil): Option[List[B]]) { (next, acc) =>
+      acc.flatMap(
+        bs => f(next).flatMap(
+          b => Some(b :: bs)
+        )
+      )
+
+  }
